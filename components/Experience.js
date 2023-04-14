9624d22b2c6862b8211a081bd9f8224b52e6a151
Aanchal Sahu <81310029+Aanchalll@users.noreply.github.com>
2023-04-14 13:10:51 +0530

    profile photo animation

--- components/Experience.js
+++ components/Experience.js
@@ -40,7 +40,7 @@ export default function Experience() {
 
 const ExperienceCard = ({ title, desc, year, company, companyLink, key }) => {
   return (
-    <div key={key} className="relative experience-card border p-4 rounded-md shadow-xl bg-[#75c1f8] dark:bg-gray-800 z-10 mx-4">
+    <div key={key} className="relative experience-card border p-4 rounded-md shadow-xl bg-[#75c1f8] dark:bg-gray-800 z-10 mx-4  animate-pulse ">
       <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-500 font-bold dark:text-gray-800">
         {year}
       </h1>
